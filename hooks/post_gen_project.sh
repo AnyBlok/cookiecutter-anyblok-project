39708ddfc6a4d5591db8d4f7f42f33744882de9c
Pierre Verkest <pverkest@anybox.fr>
2017-09-22 17:16:56 +0200

    post hooks remove pyramid test if pyramid is not required

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,4 +2,5 @@
 
 {%- if cookiecutter.http_server != 'anyblok_pyramid' %}
 rm -v {{cookiecutter.python_package}}/{{cookiecutter.blok_name}}/views.py
+rm -v {{cookiecutter.python_package}}/{{cookiecutter.blok_name}}/tests/test_pyramid.py
 {%- endif %}
