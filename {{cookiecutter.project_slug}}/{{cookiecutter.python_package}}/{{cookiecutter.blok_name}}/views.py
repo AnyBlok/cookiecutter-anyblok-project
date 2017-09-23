9b3440bc7983ef59b50018149542375fff93fcd6
Franck Bret <f.bret@sensee.com>
2017-09-23 12:28:22 +0200

    ensure a view is loaded only the related blok is installed

--- {{cookiecutter.project_slug}}/{{cookiecutter.python_package}}/{{cookiecutter.blok_name}}/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.python_package}}/{{cookiecutter.blok_name}}/views.py
@@ -1,11 +1,18 @@
-from pyramid.view import view_config
+from pyramid.view import view_defaults, view_config
+from anyblok_pyramid import current_blok
 from pyramid.response import Response
 
 
-@view_config(route_name='example')
-def example_get(request):
-    """http get view example.
-    See http://doc.anyblok-pyramid.anyblok.org/en/latest/MEMENTO.html#memento
-    and https://trypyramid.com for more examples
-    """
-    return Response("Hello world!")
+@view_defaults(installed_bloks=current_blok())
+class ExampleView:
+    def __init__(self, request):
+        self.request = request
+        self.registry = request.anyblok.registry
+
+    @view_config(route_name='example')
+    def example_get(self):
+        """http get view example.
+        See http://doc.anyblok-pyramid.anyblok.org/en/latest/MEMENTO.html#memento
+        and https://trypyramid.com for more examples
+        """
+        return Response("Hello world!")
