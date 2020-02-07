a0bcd25742093104f249f992c3bd967258cfcdea
jssuzanne <js.suzanne@gmail.com>
2020-02-07 12:47:14 +0100

    Update conf

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-{%- if 'anyblok_pyramid' not in cookiecutter.http_server.split('+') %}
+{%- if not ('pyramid' == cookiecutter.http_server or 'no' != cookiecutter.furetui) %}
 rm -v {{cookiecutter.python_package}}/{{cookiecutter.blok_name}}/views.py
 rm -v {{cookiecutter.python_package}}/{{cookiecutter.blok_name}}/tests/test_pyramid.py
+{%- endif %}
+
+{%- if 'custom' != cookiecutter.furetui %}
+rm -rv {{cookiecutter.python_package}}/backend
 {%- endif %}
